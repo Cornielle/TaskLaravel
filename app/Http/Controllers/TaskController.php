7718a656d6839ddd1dd04aefced7af01e401e979
Cornielle <briancorniellebatista@gmail.com>
2019-04-14 17:53:17 -0400

    Controller Completed

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,10 +7,8 @@ class TaskController extends Controller
 {
     public function index()
     {
-
         return Task::where('archive', 0)->orderBy('id', 'desc')->get();
     }
-
     public function archived()
     {
         return Task::where('archive', 1)->orderBy('id', 'desc')->get();
@@ -19,10 +17,25 @@ class TaskController extends Controller
     {
         $this->validate($request, ['body' => 'required|max:500']);
     }
-
     public function edit(Request $request)
     {
-        $
+        $this->validate($request, [
+            'body' => 'required|max:500'
+        ]);
+        $task = Task::findOrFail($request->id);
+        $task->body = $request->body;
+        $task->save();
+    }
+    public function archive($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->archive = ! $task->archive;
+        $task->save();
+    }
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
     }
 
 }
