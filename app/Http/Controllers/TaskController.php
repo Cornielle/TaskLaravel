62f8d8a79ca2d7dd72e525781b0d5ba47bbdb2be
Cornielle <briancorniellebatista@gmail.com>
2019-04-14 17:40:37 -0400

    Updating controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,10 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+
+        return Task::where('archive', 0)->orderBy('id', 'desc')->get();
+    }
+
+    public function archived()
+    {
+        return Task::where('archive', 1)->orderBy('id', 'desc')->get();
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, ['body' => 'required|max:500']);
+    }
+
+    public function edit(Request $request)
+    {
+        $
+    }
+
 }
